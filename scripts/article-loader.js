212a6793dfa80ca844376755fd931824e476708f
Aaron Roberts <Lumbearjack@gmail.com>
2017-07-23 21:46:51 -0400

    dynamic loading added

--- scripts/article-loader.js
+++ scripts/article-loader.js
@@ -1,11 +1,23 @@
 (function() {
+	var pageID = window.location.href.split('?=').pop()
 $.ajax({
   method: "GET",
   url: "./content.json",
   id:"1707141355",
   dataType: "json"
 }).done(function( data ) {
-	console.log(data);
-	$('#article-1707141355 .article-content').html(data[0].content);
+		console.log("pid:",pageID);
+		var article = _.where(data, {id: pageID});
+		console.log(article[0]);
+		// _.find(data, function(object) {
+		// 		console.log(object);
+		//     object.id == pageID; 
+		// });
+		$('article').attr('id', article[0].id);
+		$('article .item-imagecontainer-image').attr('src', article[0].thumbnail);
+		$('article .item-title a').text(article[0].title);
+		$('article .item-author a').text(article[0].author);
+		$('article .item-date a').text(article[0].date);
+		$('article .article-content').html(article[0].content);
 	});
 })();
--- scripts/content-loader.js
+++ scripts/content-loader.js
@@ -1,3 +1,15 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDbypG0kZK-pG-5ArT577pTQa0vpm6jNgg",
+    authDomain: "iadb-ca686.firebaseapp.com",
+    databaseURL: "https://iadb-ca686.firebaseio.com",
+    projectId: "iadb-ca686",
+    storageBucket: "",
+    messagingSenderId: "769521150154"
+  };
+  firebase.initializeApp(config);
+
+
 (function() {
 $.ajax({
   method: "GET",
@@ -6,17 +18,17 @@ $.ajax({
 }).done(function( data ) {
 
     $.each(data, function(index, article){
-      console.log("init: ", index, article);
+      // console.log("init: ", index, article);
 
       if(index === 0){
         var itemAlpha = 
         `<div id="`+article.id+`" class="item item--alpha">   
-            <a href="article.html" class="item-imagecontainer">
+            <a href="article.html?=`+article.id+`" class="item-imagecontainer">
               <img src="" alt="" class="item-imagecontainer-image">
               <div class="image-overlay"></div>
             </a>
             <div class="textbox">
-              <h2 class="item-title"><a href="article.html"></a></h2>
+              <h2 class="item-title"><a href="article.html?=`+article.id+`" ></a></h2>
               <p class="item-excerpt"></p>
               <div class="item-meta-data">
                 <span class="item-author"><a href="#authorspage"></a></span>
@@ -28,15 +40,15 @@ $.ajax({
         $('.featured-big').append(itemAlpha);
 
       }else if(index < 5){
-        console.log("else: ",index, article.id)
+        // console.log("else: ",index, article.id)
         var itemSmall =
         `<div id="`+article.id+`" class="item">  
-          <a href="article.html" class="item-imagecontainer">
+          <a href="article.html?=`+article.id+`"  class="item-imagecontainer">
             <img src="img/mh.jpeg" alt="" class="item-imagecontainer-image">
             <div class="image-overlay"></div>
           </a>
           <div class="textbox">
-            <h2 class="item-title"><a href="article.html">This is the featured title</a></h2>
+            <h2 class="item-title"><a href="article.html?=`+article.id+`" >This is the featured title</a></h2>
             <div class="item-meta-data">
               <span class="item-author"><a href="#authorspage"> Sir Author McLongName</a></span>
               <span class="item-date"><a href="#archivepage">17.06.23</a></span>
@@ -48,12 +60,12 @@ $.ajax({
       }else{
         var itemRecent =
         `<div id="`+article.id+`" class="item">  
-          <a href="article.html" class="item-imagecontainer">
+          <a href="article.html?=`+article.id+`"  class="item-imagecontainer">
             <img src="img/mh.jpeg" alt="" class="item-imagecontainer-image">
             <div class="image-overlay"></div>
           </a>
           <div class="textbox">
-            <h2 class="item-title"><a href="article.html">This is the featured title</a></h2>
+            <h2 class="item-title"><a href="article.html?=`+article.id+`" >This is the featured title</a></h2>
             <div class="item-meta-data">
               <span class="item-author"><a href="#authorspage"> Sir Author McLongName</a></span>
               <span class="item-date"><a href="#archivepage">17.06.23</a></span>
@@ -72,5 +84,8 @@ $.ajax({
       $(stringID +' .item-author a').text(article.author);
       $(stringID +' .item-date a').text(article.date);
     });
+  });
+  $('body').on('click', 'a', function(){
+    console.log(this.id);
   });
 })();
