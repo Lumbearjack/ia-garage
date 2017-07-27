40ebf6ca15281883f52333c6be064f61ebbe76ce
Aaron Roberts <>
2017-07-26 22:02:42 -0400

    re adding

--- scripts/article-loader.js
+++ scripts/article-loader.js
@@ -8,18 +8,16 @@ $.ajax({
 }).done(function( data ) {
 		console.log("pid:",pageID);
 		var article = _.where(data, {id: pageID});
-
-		//Templates
-		var moreFrom = `<hr class="divider"><p id="more-from">Read more from <a href="#authorpage" >`+article[0].author+`</a></p>`;
-		console.log(moreFrom);
-		
-		//Apply content
+		console.log(article[0]);
+		// _.find(data, function(object) {
+		// 		console.log(object);
+		//     object.id == pageID; 
+		// });
 		$('article').attr('id', article[0].id);
 		$('article .item-imagecontainer-image').attr('src', article[0].thumbnail);
 		$('article .item-title a').text(article[0].title);
 		$('article .item-author a').text(article[0].author);
 		$('article .item-date a').text(article[0].date);
 		$('article .article-content').html(article[0].content);
-		$('article .article-content').append(moreFrom);
 	});
 })();
